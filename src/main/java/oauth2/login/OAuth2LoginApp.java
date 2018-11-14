3aac0408e852d0e3a7bed8778fe6634632bdfd70
Alin Balasa <balasaalin@gmail.com>
2018-11-14 19:05:59 +0200

    no message

---
+++ src/main/java/oauth2/login/OAuth2LoginApp.java
@@ -0,0 +1,13 @@
+package oauth2.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OAuth2LoginApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2LoginApp.class, args);
+	}
+}
