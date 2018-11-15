a1e91ffa0ac752ce7c3c08347eb3f4f5c7e62cae
Alin Balasa <balasaalin@gmail.com>
2018-11-15 16:50:38 +0200

    no message

--- src/main/java/oauth2/login/LoginController.java
+++ src/main/java/oauth2/login/LoginController.java
@@ -5,17 +5,29 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ResolvableType;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class LoginController {
 
 	private static String authorizationRequestBaseUri = "oauth2/authorization";
 	Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+	
+	@Autowired
+	private OAuth2AuthorizedClientService authorizedClientService;
 
 	@Autowired
 	private ClientRegistrationRepository clientRegistrationRepository;
@@ -36,5 +48,32 @@ public class LoginController {
 	    model.addAttribute("urls", oauth2AuthenticationUrls);
 	 
 	    return "oauth_login";
+	}
+	
+	@GetMapping("/loginSuccess")
+	public String getLoginInfo(Model model, OAuth2AuthenticationToken authentication) {
+		OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(authentication.getAuthorizedClientRegistrationId(),authentication.getName());
+
+		String userInfoEndpointUri = client.getClientRegistration()
+				.getProviderDetails().getUserInfoEndpoint().getUri();
+
+		if (!StringUtils.isEmpty(userInfoEndpointUri)) {
+			RestTemplate restTemplate = new RestTemplate();
+			HttpHeaders headers = new HttpHeaders();
+			headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + client.getAccessToken()
+			.getTokenValue());
+			HttpEntity entity = new HttpEntity("", headers);
+			ResponseEntity<Map> response = restTemplate
+					.exchange(userInfoEndpointUri, HttpMethod.GET, entity, Map.class);
+			Map userAttributes = response.getBody();
+			model.addAttribute("name", userAttributes.get("name"));
+		}
+
+		return "loginSuccess";
+	}
+	
+	@GetMapping("/loginFailure")
+	public String loginFailure(Model model) {
+		return "loginFailure";
 	}
 }
--- src/main/java/oauth2/login/SecurityConfig.java
+++ src/main/java/oauth2/login/SecurityConfig.java
@@ -1,7 +1,14 @@
 package oauth2.login;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.endpoint.NimbusAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -15,6 +22,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     	      .authenticated()
     	      .and()
     	      .oauth2Login()
-    	      .loginPage("/oauth_login");
+	    	      .defaultSuccessUrl("/loginSuccess", true)
+	    	      .failureUrl("/loginFailure")
+	    	      .loginPage("/oauth_login")
+	    	      .authorizationEndpoint()
+	    	      .baseUri("/oauth2/authorize-client")
+	    	      .authorizationRequestRepository(authorizationRequestRepository())
+	    	      .and()
+	    	      .tokenEndpoint()
+	    	      .accessTokenResponseClient(accessTokenResponseClient())
+	    	      .and()
+	    	      .redirectionEndpoint()
+	    	      .baseUri("/oauth2/redirect");
+    }
+    
+    @Bean
+    public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository() {
+        return new HttpSessionOAuth2AuthorizationRequestRepository();
+    }
+    
+    @Bean
+    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
+        return new NimbusAuthorizationCodeTokenResponseClient();
     }
 }
